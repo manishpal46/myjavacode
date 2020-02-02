3804be25557a7317d3ea9d8dcc3a29b6456b1c28
root <root@ip-172-31-44-249.ap-south-1.compute.internal>
2020-02-02 13:20:46 +0000

    Added new feature to HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ class HelloWorld{
 	public static void main(String args[]){
 	 System.out.println("Hello Java");
 	
-	System.out.println("Welcome to Git");
+	 System.out.println("Welcome to Git");
+
+	 System.out.println("Added new feature");
 	}
 }
